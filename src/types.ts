678736cc439dde3fd30852217b707637399f370d
maxchaz2 <maxchaz2@proton.me>
2026-09-22 21:04:43 -0400

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export type Bindings = {
+  PLANETARY_MODE: string;
+  UMBRELLA_ENFORCEMENT: string;
+
+  IDENTITY_JWT_SECRET: string;
+  IDENTITY_JWT_ISSUER: string;
+  IDENTITY_JWT_AUDIENCE: string;
+
+  MAX_OS_1: Fetcher;
+};
