3ca1a53b83d6ed8323dfa6cc0573768849cb6ade
jurreau3 <jurreau3@proton.me>
2026-09-25 16:31:29 -0400

    Implement JWT identity verification function

---
+++ src/jwt.ts
@@ -0,0 +1,19 @@
+import { jwtVerify } from 'jose';
+
+export async function verifyIdentityJwt(
+  token: string,
+  env: {
+    IDENTITY_JWT_ISSUER: string;
+    IDENTITY_JWT_AUDIENCE: string;
+    IDENTITY_JWT_SECRET: string;
+  },
+) {
+  const secret = new TextEncoder().encode(env.IDENTITY_JWT_SECRET);
+
+  const { payload } = await jwtVerify(token, secret, {
+    issuer: env.IDENTITY_JWT_ISSUER,
+    audience: env.IDENTITY_JWT_AUDIENCE,
+  });
+
+  return payload; // { sub, email, roles, ... }
+}
